21cabf9e10f5522fa0f7969aa6779b5e7ae0b778
mcoatanhay@gmail.com <mcoatanhay@gmail.com>
2020-02-21 21:36:08 +0100

    premier commit

---
+++ help_repertoire_compose.py
@@ -0,0 +1,23 @@
+﻿#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Fichier: help_repertoire_compose.py
+# Auteur: Marc COATANHAY
+
+"""
+    Module pour écrire un fichier d'aide du module.
+"""
+
+# Import des modules
+import repertoire
+import sys
+
+# Définitions constantes et variables globales
+repertoire.filerep()
+file = open('help.txt', 'w')
+sys.stdout = file
+help(repertoire)
+file.close()
+sys.stdout = sys.__stdout__
+
+# Définitions fonctions et classes
+
